41111900259e0fa07c68b88ff666cddc29645eed
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2021-08-25 20:25:08 -0700

    Update Converse.js

--- scripts/Converse.js
+++ scripts/Converse.js
@@ -30,8 +30,8 @@ robot.hear(/delete all the files/, function(msg) {
     var dialog = switchBoard.startDialog(msg);
 
     //Provide choices for the next step, wait for the user.
-    dialog.addChoice(/yes/, function(msg2){/*Do some stuff for the yes option*/}
-    dialog.addChoice( /no/, function(msg2){/*Do some stuff for the no option*/ }
+    dialog.addChoice(/yes/, function(msg2){/*Do some stuff for the yes option*/},
+    dialog.addChoice(/no/, function(msg2){/*Do some stuff for the no option*/}
 
     //The dialog will expire after 30 seconds.
 });
