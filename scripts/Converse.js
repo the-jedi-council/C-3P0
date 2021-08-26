c4910cc471ec7902eff9d6ff9b2e3b40a32638f0
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2021-08-25 21:01:40 -0700

    Setup for initial conversation

--- scripts/Converse.js
+++ scripts/Converse.js
@@ -2,7 +2,7 @@
 //   Simple back and forth conversation with C-3P0
 //
 // Dependencies:
-//   Yes
+//   hubot-conversation
 //
 // Configuration:
 //   None
@@ -16,27 +16,6 @@
 
 
 // 
-var switchBoard = new Conversation(robot, "user", function(msg) {
-    return msg.constructor.name === "TextMessage";
-});
-
-
-
-robot.hear(/delete all the files/, function(msg) {
-
-    msg.reply('Are you really sure???');
-
-    //Start a dialog with the user that sent this message.
-    var dialog = switchBoard.startDialog(msg);
-
-    //Provide choices for the next step, wait for the user.
-    dialog.addChoice(/yes/, function(msg2){/*Do some stuff for the yes option*/},
-    dialog.addChoice(/no/, function(msg2){/*Do some stuff for the no option*/}
-
-    //The dialog will expire after 30 seconds.
-});
-
-
 
 var Conversation = require("hubot-conversation");
 module.exports = function(robot) {
