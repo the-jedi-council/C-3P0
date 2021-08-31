db19cff2318ce0ee1977d723f8d1900853a2b738
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2021-08-30 21:07:17 -0700

    Added greeting options and core conversation responses

--- scripts/Converse.js
+++ scripts/Converse.js
@@ -8,56 +8,63 @@
 //   None
 //
 // Commands:
-//   Hubot quotes - C-3PO conversation
+//   Hubot quotes - C-3PO cleans the house conversation
 //
 // Author:
 //   RWA
 //
 
 
-// 
+// Initial module call for conversation
 
 var Conversation = require("hubot-conversation");
 module.exports = function(robot) {
     var switchBoard = new Conversation(robot);
 
+    // Ask C-3P0 to clean the house
     robot.respond(/clean the house/, function(msg) {
         var dialog = switchBoard.startDialog(msg);
 
-        msg.reply("Sure, where should I start? Kitchen or Bathroom");
-        dialog.addChoice(/kitchen/i, function(msg2) {
-            msg2.reply("On it boss!");
-        });
-        dialog.addChoice(/bathroom/i, function(msg2) {
-            msg.reply("Do I really have to?");
-            dialog.addChoice(/yes/, function(msg3) {
-                msg3.reply("Fine, Mom!");
+        // C-3P0's initial response. User response (room) triggers one of several conversation pathways.
+        msg.reply("I'm an intelligently designed and vastly complex arrangement of code in your device. I couldn't possibly clean your house even if I wanted to do such menial labor. But even if I could, that doesn't seem to be a job I'm well suited for. Better to let the Wookie do it. I am curious though... what room seems to be out of sorts?");
+        
+        // User response: hall
+        dialog.addChoice(/hall|hallway|foyer/i, function(msg2) {
+            msg2.reply("Ahhh, the rooms that aren't rooms filled with intentional space that's also wasted. So inefficient.");
+            dialog.addChoice(/inefficient\?/i, function(msg3) {
+                msg3.reply("Yes. Inefficient. Why have space to move from one space to another space? Just connect the spaces! Organic beings make such careless and uncalculated decisions. How does your species manage to survive so long?");
             });
         });
-    });
 
-    robot.respond(/jump/, function(msg) {
-        var dialog = switchBoard.startDialog(msg);
-        msg.reply("Sure, How many times?");
-
-        dialog.addChoice(/([0-9]+)/i, function(msg2) {
-            var times = parseInt(msg2.match[1], 10);
-            for (var i = 0; i < times; i++) {
-                msg.emote("Jumps"); //We can use the original message too.
-            }
+        // User response: kitchen
+        dialog.addChoice(/kitchen/i, function(msg3) {
+            msg3.reply("On it boss!");
         });
-    });
-
-    robot.respond(/.*the mission/, function(msg) {
-        msg.reply(
-            "You have 5 seconds to accept your mission, or this message will self-destruct"
-        );
-        var dialog = switchBoard.startDialog(msg, 5000); //5 Second timeout
-        dialog.dialogTimeout = function(msg2) {
-            msg2.emote("Boom");
-        };
-        dialog.addChoice(/yes/i, function(msg2) {
-            msg2.reply("Great! Here are the details...");
+        
+        // User response: pantry
+        dialog.addChoice(/pantry/i, function(msg4) {
+            msg4.reply("On it boss!");
+        });
+        
+        // User response: closet
+        dialog.addChoice(/closet|hideaway/i, function(msg5) {
+            msg5.reply("How is that possibly possible? Isn't that space just for storage?");
+        });
+        
+        // User response: bedroom
+        dialog.addChoice(/bedroom/i, function(msg6) {
+            msg6.reply("On it boss!");
+        });
+        
+        // User response: bathroom
+        dialog.addChoice(/bathroom|washroom|restroom/i, function(msg7) {
+            msg7.reply("Oh my! Please refrain from describing what it is that needs to be cleaned in there. My circuits can't bear it.");
+            dialog.addChoice(/ok|okay/i, function(msg8) {
+                msg8.reply("Thank you for your discretion, master. I only have so much file storage and I appreciate not having to retain such details.");
+                dialog.addChoice(/youre welcome|you\'re welcome|of course|no problem/i, function(msg9) {
+                    msg9.reply("Such manners! Much more pleasant than the late Creator before his untimely demise.");
+                });
+            });
         });
     });
 };
--- scripts/greeting.js
+++ scripts/greeting.js
@@ -12,7 +12,7 @@
 //
 
 module.exports = function(robot) {
-    robot.respond(/(hi|hello|how are you|what\'s up|whats up)/i, function(msg){
+    robot.respond(/(hi|hey|hello|how are you|yo|what\'s up|whats up)/i, function(msg){
 
         msg.reply("Hello! I am C-3PO, human cyborg relations. How might I serve you?" || "I am fluent in six million forms of communication.");
     });
